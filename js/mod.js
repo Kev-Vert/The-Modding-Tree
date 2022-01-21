3aa8a01b75d12fb9d829fdb77126f9f938c69495
Kev-Vert <kevvert@gmail.com>
2022-01-21 13:48:19 -0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,6 +1,6 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
+	name: "The Trevtree",
+	id: "areyoustillthere",
 	author: "nobody",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
